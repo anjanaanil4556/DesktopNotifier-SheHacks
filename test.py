6824d6793c7b7b8989365339b3b331f7ac759787
anjanaanil4556 <56733610+anjanaanil4556@users.noreply.github.com>
2021-09-28 13:58:35 +0530

    final update on our code file test.py
    We completed our SheHacks project, Desktop Notifier for covid updates using python .

--- test.py
+++ test.py
@@ -1,13 +1,16 @@
 import COVID19Py
+import time
+from plyer import notification
 
 covid19=COVID19Py.COVID19(data_source="jhu")
 latest = covid19.getLatest()
+    
+while True:
 
-from plyer import notification
-notification.notify(
+    notification.notify(
     title="Covid 19 UPDATES",
     message=str(latest),
     app_icon="C:/Users/Anjana Anil/OneDrive/Desktop/Project/covid.ico",
-    timeout=30
- )
-
+    timeout=2
+  )
+    time.sleep(24*3600)
